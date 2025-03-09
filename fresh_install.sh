930920507fb8281828dc27dd5141dde3a2b22f0c
Viktor Nemeth Andersen <nemeth@nemeth-desktop.zeus>
2025-03-09 10:32:28 +0100

    more styling

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 # Install sway
-sudo apt install sway sway-backgrounds swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland
+sudo apt install sway sway-backgrounds swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland -y
 
 # Install sddm
 sudo apt install --no-install-recommends -y sddm
 sudo systemctl enable sddm
 
 # Install vim
-sudo apt install vim
+sudo apt install vim -y
 
 # Install kitty + additional dependency
-sudo apt install libwayland-egl1-mesa
-
+sudo apt install libwayland-egl1-mesa -y
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 sudo ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten /usr/bin/
 
 # Install curl
-sudo apt install curl
-
+sudo apt install curl -y
 
 # Install chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
 sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo apt update
-sudo apt install google-chrome-stable
+sudo apt install google-chrome-stable -y
 
 # Install stow
-sudo apt install stow
-
+sudo apt install stow -y
 
 # Install wofi
 sudo apt install libwayland-dev libgtk-3-dev pkgconf meson mercurial -y
